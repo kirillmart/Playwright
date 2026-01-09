915aad27e270c04d4db9436baed6ba57014bc9cb
kirillmart <kirillmartoff@gmail.com>
2026-01-09 15:25:45 +0200

    added mock tests

--- tests/mock.test.ts
+++ tests/mock.test.ts
@@ -1,21 +1,5 @@
 import { test, Page, expect } from '@playwright/test';
 
-test('Mock login API failure (500)', async ({ page }) => {
- await page.route('**/login', route => {
-   route.fulfill({
-     status: 500,
-     contentType: 'application/json',
-     body: JSON.stringify({ message: 'Internal Server Error' })
-   });
- });
- await page.goto('https://www.saucedemo.com/');
- await page.fill('#user-name', 'standard_user');
- await page.fill('#password', 'secret_sauce');
- await page.click('#login-button');
- await expect(page.locator('[data-test="error"]'))
-   .toContainText('Epic');
-});
-
 test("mocks a fruit and doesn't call api", async ({ page }) => {
   // Mock the api call before navigating
   await page.route('*/**/api/v1/fruits', async route => {
@@ -27,4 +11,36 @@ test("mocks a fruit and doesn't call api", async ({ page }) => {
 
   // Assert that the Strawberry fruit is visible
   await expect(page.getByText('Strawberry')).toBeVisible();
+});
+
+test("mocks an error and doesn't call api", async ({ page }) => {
+  // Mock the api call before navigating
+  await page.route('*/**/api/v1/fruits', async route => {
+    status: 500;
+    const json = { error: 'Internal Server Error' };
+    await route.fulfill({ json });
+  });
+  // Go to the page
+  await page.goto('https://demo.playwright.dev/api-mocking');
+
+  // Assert that error page is empty
+  await expect(page.getByText('Render a List of Fruits')).toBeHidden();
+});
+
+test("modify response", async ({ page }) => {
+  // Mock the api call before navigating
+  await page.route('*/**/api/v1/fruits', async route => {
+    const response = await route.fetch();
+    const json = await response.json();
+    json.push({ name: 'Kirill', id: 42 });
+    route.fulfill({
+      response,
+      json,
+    });
+  });
+  // Go to the page
+  await page.goto('https://demo.playwright.dev/api-mocking');
+
+  // Assert that error page is empty
+  await expect(page.getByText('Kirill')).toBeVisible() ;
 });
